584793d203dceddf497a234600118f4aa1a2bc79
Dillon Charles <dilloncharles4@gmail.com>
2020-01-16 20:08:09 -0800

    Updated OI

--- src/frc/team1983/commands/SetMotor.java
+++ src/frc/team1983/commands/SetMotor.java
@@ -1,12 +1,13 @@
 package frc.team1983.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.team1983.util.motors.ControlMode;
 import frc.team1983.util.motors.Motor;
 
 public class SetMotor extends InstantCommand
 {
-	public SetMotor(Motor motor, double throttle)
+	public SetMotor(Motor motor, ControlMode controlMode, double value)
 	{
-		super(() -> motor.set(throttle));
+		super(() -> motor.set(controlMode, value));
 	}
 }
--- src/frc/team1983/services/OI.java
+++ src/frc/team1983/services/OI.java
@@ -14,6 +14,7 @@ public class OI
         PANEL(2);
 
         private int port;
+
         Joysticks(int port)
         {
             this.port = port;
@@ -25,10 +26,20 @@ public class OI
         }
     }
 
-    public static final double JOYSTICK_DEADZONE = 0.15;
+    protected static final double JOYSTICK_DEADZONE = 0.15;
+    protected static final double JOYSTICK_EXPONENT = 1.7;
+    protected static final double LINEAR_ZONE = 0.4;
+    protected static final double LINEAR_SLOPE = Math.abs(Math.pow(LINEAR_ZONE, JOYSTICK_EXPONENT) / (LINEAR_ZONE - JOYSTICK_DEADZONE));
 
     private Joystick left, right, panel;
     private HashMap<Joysticks, HashMap<Integer, JoystickButton>> buttons;
+
+    protected static double scale(double raw)
+    {
+        if(Math.abs(raw) < JOYSTICK_DEADZONE) return 0;
+        if(Math.abs(raw) < LINEAR_ZONE) return (LINEAR_SLOPE * raw) - (Math.signum(raw) * JOYSTICK_DEADZONE);
+        else return Math.pow(Math.abs(raw), JOYSTICK_EXPONENT) * Math.signum(raw);
+    }
 
     public OI(Joystick left, Joystick right, Joystick panel, HashMap<Joysticks, HashMap<Integer, JoystickButton>> buttons)
     {
@@ -41,18 +52,10 @@ public class OI
     public OI()
     {
         this(new Joystick(Joysticks.LEFT.getPort()),
-            new Joystick(Joysticks.RIGHT.getPort()),
-            new Joystick(Joysticks.PANEL.getPort()),
-            new HashMap<>()
+                new Joystick(Joysticks.RIGHT.getPort()),
+                new Joystick(Joysticks.PANEL.getPort()),
+                new HashMap<>()
         );
-    }
-
-    private double scale(double raw)
-    {
-        if(Math.abs(raw) < JOYSTICK_DEADZONE)
-            return 0;
-
-        return raw > 0 ? raw * raw : -(raw * raw);
     }
 
     public double getLeftY()
@@ -65,14 +68,14 @@ public class OI
         return scale(-right.getY());
     }
 
-    public double getLeftX()
-    {
-        return scale(-left.getX());
-    }
-
     public double getRightX()
     {
-        return scale(-right.getX());
+        return scale(right.getX());
+    }
+
+    public double getLeftX()
+    {
+        return left.getX();
     }
 
     public JoystickButton getButton(Joysticks joystickPort, int button)
@@ -86,7 +89,7 @@ public class OI
             case RIGHT:
                 joystick = right;
                 break;
-            default:
+            default: // If it wasn't the other two it must be panel. Java doesn't like it if we just do case PANEL.
                 joystick = panel;
                 break;
         }
@@ -98,7 +101,6 @@ public class OI
 
         return buttons.get(joystickPort).get(button);
     }
-
     public void initializeBindings()
     {
 
