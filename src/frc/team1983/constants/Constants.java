dcecdf6ff7d8c43dd5cd15eb20a68aef5ee871c8
KaiNakamura <kai@nakamura-online.com>
2020-02-10 20:33:31 -0800

    New poses, increased FollowTrajectory speed

--- src/frc/team1983/constants/Constants.java
+++ src/frc/team1983/constants/Constants.java
@@ -23,6 +23,11 @@ public class Constants
             Units.feetToMeters(-94.66 / 12.0),
             new Rotation2d(Units.degreesToRadians(180))
         );
+        public static final Pose2d START_IN_FRONT_OF_OPPONENT_TRENCH_RUN = new Pose2d(
+            Units.feetToMeters(10 + ROBOT_LENGTH / 2.0),
+            Units.feetToMeters(-295.5 / 12.0),
+            new Rotation2d(Units.degreesToRadians(180))
+        );
 
         public static final Pose2d TRENCH_RUN_BALL_1 = new Pose2d(
             Units.feetToMeters(242.63 / 12.0 /*- ROBOT_LENGTH / 2.0*/),
@@ -33,6 +38,26 @@ public class Constants
             Units.feetToMeters(378.9 / 12.0 - ROBOT_LENGTH / 2.0),
             Units.feetToMeters(-27.75 / 12.0),
             new Rotation2d(Units.degreesToRadians(180))
+        );
+        public static final Pose2d RENDEZVOUS_POINT_BALL_1 = new Pose2d(
+            Units.feetToMeters(240.51 / 12.0 - ROBOT_LENGTH / 2.0 * Math.cos(Units.degreesToRadians(22.5))),
+            Units.feetToMeters(-175.8 / 12.0 - ROBOT_LENGTH / 2.0 * Math.sin(Units.degreesToRadians(22.5))),
+            new Rotation2d(Units.degreesToRadians(-157.5))
+        );
+        public static final Pose2d RENDEZVOUS_POINT_BALL_2 = new Pose2d(
+            Units.feetToMeters(234.17 / 12.0 - ROBOT_LENGTH / 2.0 * Math.cos(Units.degreesToRadians(22.5))),
+            Units.feetToMeters(-160.5 / 12.0 - ROBOT_LENGTH / 2.0 * Math.sin(Units.degreesToRadians(22.5))),
+            new Rotation2d(Units.degreesToRadians(-157.5))
+        );
+        public static final Pose2d RENDEZVOUS_POINT_BALL_3 = new Pose2d(
+            Units.feetToMeters(227.83 / 12.0 - ROBOT_LENGTH / 2.0 * Math.cos(Units.degreesToRadians(22.5))),
+            Units.feetToMeters(-145.2 / 12.0 - ROBOT_LENGTH / 2.0 * Math.sin(Units.degreesToRadians(22.5))),
+            new Rotation2d(Units.degreesToRadians(-157.5))
+        );
+        public static final Pose2d RENDEZVOUS_POINT_BALL_1_AND_2_AND_3 = new Pose2d(
+            RENDEZVOUS_POINT_BALL_1.getTranslation().getX() + 0.25 * (RENDEZVOUS_POINT_BALL_3.getTranslation().getX() - RENDEZVOUS_POINT_BALL_1.getTranslation().getX()),
+            RENDEZVOUS_POINT_BALL_1.getTranslation().getY() + 0.25 * (RENDEZVOUS_POINT_BALL_3.getTranslation().getY() - RENDEZVOUS_POINT_BALL_1.getTranslation().getY()),
+            new Rotation2d(Units.degreesToRadians(-157.5))
         );
         public static final Pose2d RENDEZVOUS_POINT_BALL_4 = new Pose2d(
             Units.feetToMeters(234.94 / 12.0 - ROBOT_LENGTH / 2.0 * Math.sin(Units.degreesToRadians(22.5))),
@@ -52,6 +77,11 @@ public class Constants
         public static final Pose2d RENDEZVOUS_TRENCH_RUN_SWITCH = new Pose2d(
             Units.feetToMeters(242.63 / 12.0 - 6.0),
             Units.feetToMeters(-27.75 / 12.0),
+            new Rotation2d(Units.degreesToRadians(180))
+        );
+        public static final Pose2d OPPONENT_TRENCH_RUN_BALL_4_AND_5 = new Pose2d(
+            Units.feetToMeters(250.35 / 12.0 - ROBOT_LENGTH / 2.0),
+            Units.feetToMeters(-295.5 / 12.0),
             new Rotation2d(Units.degreesToRadians(180))
         );
     }
--- test/frc/team1983/constants/UT_Pose.java
+++ test/frc/team1983/constants/UT_Pose.java
@@ -13,11 +13,18 @@ public class UT_Pose
     public void printPoses()
     {
         System.out.println("START_IN_FRONT_OF_TRENCH_RUN: " + Constants.Pose.START_IN_FRONT_OF_TRENCH_RUN);
+        System.out.println("START_IN_FRONT_OF_POWER_PORT: " + Constants.Pose.START_IN_FRONT_OF_POWER_PORT);
+        System.out.println("START_IN_FRONT_OF_OPPONENT_TRENCH_RUN: " + Constants.Pose.START_IN_FRONT_OF_OPPONENT_TRENCH_RUN);
         System.out.println("TRENCH_RUN_BALL_1: " + Constants.Pose.TRENCH_RUN_BALL_1);
         System.out.println("TRENCH_RUN_BALL_4_AND_5: " + Constants.Pose.TRENCH_RUN_BALL_4_AND_5);
+        System.out.println("RENDEZVOUS_POINT_BALL_1: " + Constants.Pose.RENDEZVOUS_POINT_BALL_1 + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_1.getRotation().getCos() + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_1.getRotation().getSin());
+        System.out.println("RENDEZVOUS_POINT_BALL_2: " + Constants.Pose.RENDEZVOUS_POINT_BALL_2 + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_2.getRotation().getCos() + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_2.getRotation().getSin());
+        System.out.println("RENDEZVOUS_POINT_BALL_3: " + Constants.Pose.RENDEZVOUS_POINT_BALL_3 + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_3.getRotation().getCos() + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_3.getRotation().getSin());
+        System.out.println("RENDEZVOUS_POINT_BALL_1_AND_2_AND_3: " + Constants.Pose.RENDEZVOUS_POINT_BALL_1_AND_2_AND_3 + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_1_AND_2_AND_3.getRotation().getCos() + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_1_AND_2_AND_3.getRotation().getSin());
         System.out.println("RENDEZVOUS_POINT_BALL_4: " + Constants.Pose.RENDEZVOUS_POINT_BALL_4 + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_4.getRotation().getCos() + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_4.getRotation().getSin());
         System.out.println("RENDEZVOUS_POINT_BALL_5: " + Constants.Pose.RENDEZVOUS_POINT_BALL_5 + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_5.getRotation().getCos() + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_5.getRotation().getSin());
         System.out.println("RENDEZVOUS_POINT_BALL_4_AND_5: " + Constants.Pose.RENDEZVOUS_POINT_BALL_4_AND_5 + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_4_AND_5.getRotation().getCos() + ", " + Constants.Pose.RENDEZVOUS_POINT_BALL_4_AND_5.getRotation().getSin());
+        System.out.println("OPPONENT_TRENCH_RUN_BALL_4_AND_5: " + Constants.Pose.OPPONENT_TRENCH_RUN_BALL_4_AND_5);
     }
 
     @Test
