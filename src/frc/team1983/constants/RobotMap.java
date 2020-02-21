54363206adfc02bbf5e94a9c4e9c467acee55dcd
Naba <nabadeyab@gmail.com>
2020-02-20 22:41:00 -0800

    fixed motor constants

--- src/frc/team1983/constants/RobotMap.java
+++ src/frc/team1983/constants/RobotMap.java
@@ -29,7 +29,7 @@ public class RobotMap //TODO: all of these values have yet to be finalized
         public static final int ACCELERATOR_1 = 16;
         public static final boolean ACCELERATOR_1_REVERSED = false;
 
-        public static final int ACCELERATOR_2 = 2;
+        public static final int ACCELERATOR_2 = 15;
         public static final boolean ACCELERATOR_2_REVERSED = true;
 
         public static final int FLYWHEEL_1 = 3;
@@ -38,24 +38,21 @@ public class RobotMap //TODO: all of these values have yet to be finalized
         public static final int FLYWHEEL_2 = 12;
         public static final boolean FLYWHEEL_2_REVERSED = true;
 
-        public static final int ARTICULATION_4 = 0;
+        public static final int ARTICULATION_1 = 4;
         public static final boolean ARTICULATION_1_REVERSED = true;
 
-        public static final int ARTICULATION_2 = 11;
-        public static final boolean ARTICULATION_2_REVERSED = false;
+        //public static final int ARTICULATION_2 = 11;
+        //public static final boolean ARTICULATION_2_REVERSED = false;
     }
 
     public static class Indexer
     {
         public static final int SENSOR = 0;
 
-        public static final int COLLECTOR_TRANSFER = 0;
+        public static final int COLLECTOR_TRANSFER = 5;
         public static final boolean COLLECTOR_TRANSFER_REVERSED = false;
 
-        public static final int INTERNAL_SHUTTLE = 0;
-        public static final boolean INTERNAL_SHUTTLE_REVERSED = false;
-
-        public static final int SHOOTER_TRANSFER = 0;
+        public static final int SHOOTER_TRANSFER = 10;
         public static final boolean SHOOTER_TRANSFER_REVERSED = false;
     }
 
@@ -70,7 +67,7 @@ public class RobotMap //TODO: all of these values have yet to be finalized
 
     public static class ControlPanel
     {
-        public static final int ROLLER = 0;
+        public static final int ROLLER = 9;
         public static final boolean ROLLER_REVERSED = false;
 
         public static final int PISTON_FORWARD = 0;
