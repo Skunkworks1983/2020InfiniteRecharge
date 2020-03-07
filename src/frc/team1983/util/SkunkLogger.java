e6703bdd2d9518a98bf492df9818ab36b968b535
Naba <nabadeyab@gmail.com>
2020-03-07 15:18:28 -0800

    logger, without unit tests (yet)

--- src/frc/team1983/util/SkunkLogger.java
+++ src/frc/team1983/util/SkunkLogger.java
@@ -1,4 +1,23 @@
 package frc.team1983.util;
 
-public class SkunkLogger {
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class SkunkLogger
+{
+    private String className;
+
+    public SkunkLogger (Object object)
+    {
+        this.className = object.getClass().getSimpleName();
+    }
+
+    public synchronized void logMessage (String message)
+    {
+        String now = DateTimeFormatter.ofPattern("MM-dd HH:mm:ss.SSS").
+                withZone(ZoneId.systemDefault()).format(Instant.now());
+
+        System.out.println(now + " " + className + " " + message);
+    }
 }
