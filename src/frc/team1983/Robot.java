148983583376229163d07b3657b83612f1bbd6d1
Dillon Charles <dilloncharles4@gmail.com>
2020-03-08 14:02:24 -0700

    Updated shooter articulation safety constants

--- src/frc/team1983/Robot.java
+++ src/frc/team1983/Robot.java
@@ -60,6 +60,7 @@ public class Robot extends TimedRobot
     public void robotPeriodic()
     {
         CommandScheduler.getInstance().run();
+        System.out.println(shooter.getArticulationPosition());
     }
 
 	@Override
